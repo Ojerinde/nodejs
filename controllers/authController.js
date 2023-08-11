ee81c55c6174c85701c8a0b24767f786f4f7e150
Joel Ojerinde <joelojerinde@gmail.com>
2023-08-11 19:37:14 +0100

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser
+    }
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  // Check if the user exist and the password is correct.
+  // For a field that is not selected by default, we need to use '+password'
+  const user = User.findOne({ email: email }).select('+password');
+
+  // Send token if everything is ok
+
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token
+  });
+});
---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Email is not valid']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    min: [8, 'MinLength should be 8'],
+    select: false
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Password is required'],
+    validate: {
+      // custom validator. works on save and create.
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Password does not match'
+    }
+  }
+});
+
+// AFter the schema has been validated but befor the document is saved in the database
+userSchema.pre('save', async function(next) {
+  // Only run if password was actually modify
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 1);
+
+  // confirmPassword should not be persited to the database.
+  this.confirmPassword = undefined;
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
