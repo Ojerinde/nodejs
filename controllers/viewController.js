39672e8f383bd7b67e5a67ad7d2b336b71ab3bea
Joel Ojerinde <joelojerinde@gmail.com>
2023-08-28 17:40:25 +0100

    feat: integrate mapBox

---
+++ controllers/viewController.js
@@ -0,0 +1,61 @@
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // Get tour data from colection
+  const tours = await Tour.find();
+
+  // Build template
+
+  // Render the tamplate using the tour data from step one
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const { slug } = req.params;
+
+  const tour = await Tour.findOne({ slug: slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour
+  });
+});
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account'
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser
+  });
+});
